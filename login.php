437874c67a955dbe270736138028c4446871623a
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-07-28 20:34:52 +0600

    Login page active

--- login.php
+++ login.php
@@ -13,7 +13,7 @@
         <h3>Login</h3>
         <br>
 
-        <form action="home.php" method="post">
+        <form action="#" method="post">
             <div>
                 <label for="UserName">User Name</label>
                 <br>
@@ -25,24 +25,67 @@
                 <input type="password" name="password" placeholder="Enter Your Password" required> 
             </div>
             <div>
-                <input class="sbtn" type="submit" name="signup" value="Login" required>
+                <input class="sbtn" type="submit" name="login" value="Login" required>
             </div>
-        <br>
-        <br>
             <p>Create an account  <a href="signup.php"> Signup Here</a> </p>
-
         </form>
-
-
-
     </div>
 </body>
 </html>
+
 
 <?php
 
 include "connection.php";
 
+if(isset($_POST['login']))
+{
+    $username = $_POST['username'];
+    $pass = $_POST['password'];
 
+    // $find =0;
+    // // $match = "SELECT * FROM `userlist` WHERE `username` = '$username' and `password`=  $password" ; 
+    // $selectquery = "Select * from userlist";
+
+    // $qery = mysqli_query( $con , $selectquery);
+    // $num =  mysqli_num_rows($qery);
+    // $res = mysqli_fetch_array($qery);
+
+    // while ($num>=0)
+    // {
+    //     if($res['username'] == $username) 
+    //     {
+    //         $find = 1;
+    //         echo "Data find";
+    //     }
+
+    //     $num--;
+    // }
+  
+
+    $query = "SELECT * FROM userlist WHERE username = '$username' " ;
+    $result = mysqli_query($con, $query);
+    
+    if (mysqli_num_rows($result) == 1) {
+        $row = mysqli_fetch_assoc($result);
+        $hashedPassword = $row['password'];
+        if($hashedPassword == $pass)
+        {
+            header("Location: home.php");  
+        }
+        else {
+            echo "Invalid password.";
+        }
+       
+    } 
+    else {
+        echo "User not found.";
+    }
+
+
+
+
+
+}
 
 ?>
