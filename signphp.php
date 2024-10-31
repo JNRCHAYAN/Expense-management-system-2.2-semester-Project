a9e9baa3eca9cedec3835fd5e635a46b181f9adf
kheya398 <kheyaroy018@gmail.com>
2024-11-01 00:15:00 +0600

    login

--- signphp.php
+++ signphp.php
@@ -3,15 +3,14 @@
 include 'dbcon.php';
 if(isset($_POST['submit']))
 {
-    $name = mysqli_real_escape_string( $con, $_POST['name']);
+   
    
     $username = mysqli_real_escape_string($con, $_POST['username']);
    
     $email =mysqli_real_escape_string($con, $_POST['email']);
     $password = mysqli_real_escape_string($con,$_POST['password']);
-    $date = mysqli_real_escape_string($con,$_POST['birth_date']);
-    $adress =mysqli_real_escape_string($con, $_POST['adress']);
-    $gender =mysqli_real_escape_string($con, $_POST['gender']);
+   
+
 
     $pass =password_hash($password, PASSWORD_BCRYPT);
     $emailquary = " select * from registration where email='$email'";
@@ -22,9 +21,7 @@ if(isset($_POST['submit']))
     {
         echo "email already exits";
     }
-    else{
-        $insertquary = "insert into registration(id, name, username, email, password, date_of_birth, address, gender, signup, login) values('')"
-    }
+    
 }
 
 ?>
