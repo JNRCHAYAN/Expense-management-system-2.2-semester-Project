02b7c35075c0381e70880d2b903d825b66f426cf
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-11-02 01:01:14 +0600

    Update

--- dbcon.php
+++ dbcon.php
@@ -4,19 +4,4 @@ $password = "";
 $server = 'localhost';
 $db = 'expense_management';
 $con = mysqli_connect($server,$username,$password,$db);
-// if($con)
-// {
-   
-//     ?>
-//     <script>
-//         alert('Connection Successful');
-//     </script>
-//     <?php
-// }
-// else
-// {
-//  echo "No Connection";
-//  die("No connection" .mysqli_connect_error());
-
-// }
-// ?>
+?>
