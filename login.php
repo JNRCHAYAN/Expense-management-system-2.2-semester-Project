67e1288498d6a62fa476caf86cd148cc1d58544b
kheya398 <kheyaroy018@gmail.com>
2024-11-01 21:08:09 +0600

    login

--- login.php
+++ login.php
@@ -1,3 +1,14 @@
+
+<?php
+session_start();
+
+?>
+
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,23 +18,63 @@
     <link rel="stylesheet" href="loginn.css">
 </head>
 <body>
+
+<?php
+include'dbcon.php';
+if(isset($_POST['submit']))
+{
+$username=$_POST['username'];
+$password= $_POST['password'];
+$name_search=(select * from registration where username ='$username');
+$quary=mysqli_query($con,$name_search);
+$name_count=mysqli_num_rows($quary);
+
+if($name_count)
+{
+   $name_pass =mysql_fetch_assoc($quary);
+   $db_pass=$name_pass['password'];
+
+   $pass_decode= password_verify($password, $db_pass);
+
+   if( $pass_decode)
+   {
+
+    echo "login successfull";
+
+   }
+   else 
+   {
+    echo "password incurret";
+   }
+}
+else 
+{
+    echo "invalid username";
+}
+}
+
+?>
+
+
+
+
     <div class="log">
-        <form action="">
+        <form action="<?php echo htmlentities($_SERVER['PHP_SELF'])?>;" method="post">
             <h1>Login</h1>
 
             <label for="">Username</label>
             <br>
-            <input type="text" placeholder="">
+            <input type="text" name="username"placeholder="">
             <br>
 
             <label for="">Password</label>
             <br>
-            <input type="text" name="" id="" placeholder="">
+            <input type="text" name="password" id="" placeholder="">
             <br>
         
         <label for=""><input type="checkbox">Remember me</label>
         <br>
-        <button>LOGIN</button>
+        <button type="submit" name="submit">LOGIN</button>
             <br>
         </form>
     </div> 
