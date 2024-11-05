6cba41ea33cc23637733494f289ac53b6c45afea
AbuhenaHossen <sagorikahossen062@gmail.com>
2024-11-05 12:23:01 +0600

    update

--- profileedit.php
+++ profileedit.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Expense Management System - Profile Edit</title>
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/profileedit.css">
 </head>
 <body>
     <div class="container">
