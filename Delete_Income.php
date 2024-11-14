83d15931fb07d509f1a21867583f0216a1e3ff66
JNRCHAYAN <jnrchayan@gmail.com>
2024-11-15 00:53:42 +0600

    Update

---
+++ Delete_Income.php
@@ -0,0 +1,12 @@
+<?php
+include 'dbcon.php';
+
+$income_id = $_GET['income_id'];
+
+$deletq = "DELETE FROM `income` WHERE income_id =$income_id" ;
+
+$qery = mysqli_query( $con , $deletq);
+
+header('location:income.php');
+
+?>
