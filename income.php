fdac7871994af6dbfcea960aff6eea94d69bb586
kheya398 <kheyaroy018@gmail.com>
2024-11-22 00:05:46 +0600

    login

--- income.php
+++ income.php
@@ -72,11 +72,15 @@ $sett=$fach['Amount'];
                 <li><a href="#"><span class="icon">🔒</span> Profile</a></li>
                 <li><a href="#"><span class="icon">⚙️</span> Settings</a></li>
             
+
+
              <div class="log">
              
              <a href="logout.php">Logout</a>
              </div>
         
+
+             
             </ul>
         </div>
 
